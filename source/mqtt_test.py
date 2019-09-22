5fc674d8213a7966c11893e9ae52ee3511f01901
samora-j <marcus@samora.se>
2019-09-22 23:55:44 +0200

    binary sensor payload

--- source/mqtt_test.py
+++ source/mqtt_test.py
@@ -4,10 +4,11 @@ import time
 import json
 import random
 import OPi.GPIO as GPIO
+from datetime import datetime
 
-topic = 'temperature/dht22'
-sleep_time = 0.1
-
+topic = 'home-assistant/opiz/pir-1'
+on_sleep_time = 7
+off_sleep_time = 0.01
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
@@ -22,13 +23,20 @@ client.loop_start()
 GPIO.setmode(GPIO.SUNXI)
 GPIO.setup('PA16', GPIO.IN, pull_up_down=GPIO.PUD_UP)
 last_state = 1
+sleep_time = off_sleep_time
 while True:
     new_state = GPIO.input('PA16')
-    if last_state != new_state and new_state:
-        print(new_state)
-        data = {'temperature': random.randint(-20, 40),
-                'humidity': random.randint(0, 100)}
-        client.publish(topic, json.dumps(data))
-        print('Published...')
+    if last_state != new_state:
+        now = datetime.now()
+        date_time = now.strftime("%Y-%m-%d, %H:%M:%S")
+        print('Motion detected: ',date_time) 
+        if new_state == 1:
+            sleep_time = on_sleep_time
+            payload = 'ON'
+        else:
+            sleep_time = off_sleep_time
+            payload = 'OFF'
+        client.publish(topic, payload)
+        print('State published: ', payload)
     last_state = new_state
     time.sleep(sleep_time)
